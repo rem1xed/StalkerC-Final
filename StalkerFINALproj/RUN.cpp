d54a43e9e2f652506c409406bc437e8cd8dd82f4
marker37 <mr.pertak@gmail.com>
2024-06-27 19:25:30 +0300

    added mimi games

--- StalkerFINALproj/RUN.cpp
+++ StalkerFINALproj/RUN.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+#include <Windows.h>
+#include "games.h"
 using namespace std;
+
+
+
 
 void main()
 {
+	SetConsoleCP(1251); // встановлення кодування Windows-1251 в  потік введення
+	SetConsoleOutputCP(1251); // встановлення кодування Windows-1251 в  потік виведення
+
+
+
+	int SuccessInTheQuest=1;
+
+	//SuccessInTheQuest = guesTsheWord();
+
+	//playRockPaperScissors();
+
+	playStalkeroulette();
 
 }
